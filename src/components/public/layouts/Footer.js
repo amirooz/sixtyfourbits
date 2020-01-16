f9bc21fba4d2cfffb7a6d7f1acc8002e8c75fa60
MD. AMIR HOSSAIN <amirhossain0606@gmail.com>
2020-01-16 11:15:10 +0600

    Update Footer.js

--- src/components/public/layouts/Footer.js
+++ src/components/public/layouts/Footer.js
@@ -81,7 +81,7 @@ class Footer extends Component {
                                             <li><Link to="/about" className="base-text">About</Link></li>
                                             <li><Link to="/contact" className="base-text">Contact</Link></li>
                                             <li><Link to="/" className="base-text">Security</Link></li>
-                                            <li><Link to="/" className="base-text">Investors</Link></li>
+                                            <li><Link to="/" className="base-text">FAQ</Link></li>
                                             <li><Link to="/" className="base-text">News</Link></li>
                                             <li><Link to="/" className="base-text">Events</Link></li>
                                             <li><Link to="/" className="base-text">Gallery</Link></li>
@@ -90,41 +90,44 @@ class Footer extends Component {
                                     </div>
                                 </li>
                                 <li>
-                                    <div className="collapsible-header base-text">Services</div>
+                                    <div className="collapsible-header base-text"><strong>Expertise</strong></div>
                                     <div className="collapsible-body base-text">
                                         <ul>
-                                            <li><Link to="/about" className="base-text">About</Link></li>
-                                            <li><Link to="/contact" className="base-text">Contact</Link></li>
-                                            <li><Link to="/" className="base-text">Security</Link></li>
-                                            <li><Link to="/" className="base-text">Investors</Link></li>
-                                            <li><Link to="/" className="base-text">News</Link></li>
-                                            <li><Link to="/" className="base-text">Events</Link></li>
-                                            <li><Link to="/" className="base-text">Gallery</Link></li>
-                                            <li><Link to="/" className="base-text">Careers</Link></li>
+                                            <li><Link to="/expertise#cloud" className="base-text">Cloud & DevOps</Link></li>
+                                            <li><Link to="/expertise#ecommerce" className="base-text">E-commerce & Marketplace</Link></li>
+                                            <li><Link to="/expertise#opensource" className="base-text">OpenSource Integrations</Link></li>
+                                            <li><Link to="/expertise#mobile-app" className="base-text">Mobile App Development</Link></li>
+                                            <li><Link to="/expertise#machine-ai" className="base-text">Machine Learning and AI</Link></li>
+                                            <li><Link to="/expertise#seo" className="base-text">Search Engine Optimization</Link></li>
+                                            <li><Link to="/expertise#erp" className="base-text">ERP Development</Link></li>
                                         </ul>
                                     </div>
                                 </li>
                                 <li>
-                                    <div className="collapsible-header base-text">Products</div>
+                                    <div className="collapsible-header base-text"><strong>Partners</strong></div>
                                     <div className="collapsible-body base-text">
                                         <ul>
-                                            <li><Link to="/about" className="base-text">About</Link></li>
-                                            <li><Link to="/contact" className="base-text">Contact</Link></li>
-                                            <li><Link to="/" className="base-text">Security</Link></li>
-                                            <li><Link to="/" className="base-text">Investors</Link></li>
-                                            <li><Link to="/" className="base-text">News</Link></li>
-                                            <li><Link to="/" className="base-text">Events</Link></li>
-                                            <li><Link to="/" className="base-text">Gallery</Link></li>
-                                            <li><Link to="/" className="base-text">Careers</Link></li>
+                                            <li><Link to="/partner#aws" className="base-text">AWS</Link></li>
+                                            <li><Link to="/partner#gcp" className="base-text">Google Cloud Platform</Link></li>
+                                            <li><Link to="/partner#azure" className="base-text">Microsoft Azure</Link></li>
+                                            <li><Link to="/partner#magento" className="base-text">Magento</Link></li>
+                                            <li><Link to="/partner#shopify" className="base-text">Shopify</Link></li>
+                                            <li><Link to="/partner#paypal" className="base-text">PayPal</Link></li>
                                         </ul>
                                     </div>
                                 </li>
                                 <li>
-                                    <div className="collapsible-header base-text">Partner</div>
-                                    <div className="collapsible-body">
-                                        <div className="badge">
-                                            <img src={Magento} alt="Magento Partner" />
-                                        </div>
+                                    <div className="collapsible-header base-text"><strong>Get In</strong></div>
+                                    <div className="collapsible-body base-text">
+                                        <ul>
+                                            <li><Link to="/" className="base-text">Facebook</Link></li>
+                                            <li><Link to="/" className="base-text">Instagram</Link></li>
+                                            <li><Link to="/" className="base-text">Twitter</Link></li>
+                                            <li><Link to="/" className="base-text">Snapchat</Link></li>
+                                            <li><Link to="/" className="base-text">Youtube</Link></li>
+                                            <li><Link to="/" className="base-text">Linkedin</Link></li>
+                                            <li><Link to="/" className="base-text">Skype</Link></li>
+                                        </ul>
                                     </div>
                                 </li>
                             </ul>
@@ -134,7 +137,7 @@ class Footer extends Component {
                 <div className="footer-copyright">
                     <div className="container base-text">
                         <span className="copyright">
-                            © Sixtyfourbits 2020
+                            © Sixtyfourbits 2019
                         </span>
                         <span className="right hide-on-med-and-down">
                             <Link to="/" className="base-text">Sitemap</Link>
